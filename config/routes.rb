770d4b5be80905abc2f67d8b27e68899f02de3ca
野村まどか <nomuramadoka@nomuramadokanoMacBook-ea.local>
2018-12-04 23:42:20 +0900

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'quetions/show'
+  # get 'quetions/new'
+  # get 'quetions/edit'
+
+  root 'questions#index'
+  resources :questions
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
