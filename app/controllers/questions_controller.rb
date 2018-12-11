07e68ff42cfef364c35fad0398bc4118dfed001a
野村まどか <nomuramadoka@nomuramadokanoMacBook-ea.local>
2018-12-11 17:16:24 +0900

    Create question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,24 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  # newアクション
   def new
     @question = Question.new
   end
+
+  # createアクション
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save error!'
+    end
+  end
+
+  # Strong paramater: フォームから送ったデータの指定のデータだけを使うようにする（これ以外は無視する）
+  private
+    def question_params
+      params.require(:question).permit(:name, :title, :content)
+    end
 end
