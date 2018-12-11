991d3272d3484b9c938087804cdf63871b2467b4
野村まどか <nomuramadoka@nomuramadokanoMacBook-ea.local>
2018-12-11 23:47:08 +0900

    CRUD finish

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,11 +11,36 @@ class QuestionsController < ApplicationController
   # createアクション
   def create
     @question = Question.new(question_params)
-    if @question.save
+    if @question.save # saveメソッドでデータベースにデータを保存する
+      redirect_to root_path, notice: 'Success!' #flash[:notice]
+    else
+      flash[:alert] = 'Save error!'
+      render :new # :newでnewのページに入力したままの状態で戻る
+    end
+  end
+
+  # 質問編集
+  def edit
+    # URLからidはparamsに入っているのでそれを元に検索
+    @question = Question.find(params[:id])
+  end
+
+  # 編集した質問を更新する
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
       redirect_to root_path, notice: 'Success!'
     else
       flash[:alert] = 'Save error!'
+      render :edit
     end
+  end
+
+  # 削除
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to root_path, notice: 'Delete Success!'
   end
 
   # Strong paramater: フォームから送ったデータの指定のデータだけを使うようにする（これ以外は無視する）
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  validates :name, presence: true  # nameカラムを入力必須にする
+  validates :title, presence: true # titleカラムを入力必須にする
+  validates :content, presence: true # contentカラムを入力必須にす
 end
